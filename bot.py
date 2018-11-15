04acdc75cbbf26aed91978040bb89fde906413ee
ash#0001 <thomasbass2001@gmail.com>
2018-11-15 20:04:08 +0000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,44 @@
+__authors__ = 'electric-blue-green'
+__license__ = 'MIT'
+import discord
+from discord.ext import commands
+import sqlite3
+import os
+import subprocess
+import traceback
+import sys
+
+initial_extensions=['cog']
+
+def gettoken():
+    tokenfile = open("token.txt", "r")
+    tokenstring = tokenfile.read()
+    tokentoken = tokenstring.split("\n")
+    token = str(tokentoken[0])
+    return token
+token = gettoken()
+
+description = "use -help"
+
+
+
+bot = commands.Bot(command_prefix=["-"], description=description)
+
+
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print("Failed to load" + extension, file=sys.stderr)
+            traceback.print_exc()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+bot.run(token)
