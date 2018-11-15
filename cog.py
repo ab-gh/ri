297bee9be76b1e21b67600a5baf19b87fb176038
aejb <thomas.bass2001@gmail.com>
2018-11-15 21:06:14 +0000

    ksajfg

--- cog.py
+++ cog.py
@@ -101,27 +101,38 @@ class shellcog:
                 embed.add_field(name="Reconnect queue", value=(len(queue)))
                 #print("In recon queue\t", "{0:0=3d}".format(len(queue)), getstr(queue))
             if len(waitcon) != 0:
-                print("Wait to recon\t", "{0:0=3d}".format(len(waitcon)), self.getstr(waitcon))
+                #print("Wait to recon\t", "{0:0=3d}".format(len(waitcon)), self.getstr(waitcon))
+                embed.add_field(name="Waiting to reconnect", value=(len(waitcon)))
             if len(load) != 0:
-                print("Loading subs\t", "{0:0=3d}".format(len(load)), self.getstr(load))
+                #print("Loading subs\t", "{0:0=3d}".format(len(load)), self.getstr(load))
+                embed.add_field(name="Loading subsystems", value=(len(load)))
             if len(awaiting) != 0:
-                print("Awaiting conf\t", "{0:0=3d}".format(len(awaiting)), self.getstr(awaiting))
+                #print("Awaiting conf\t", "{0:0=3d}".format(len(awaiting)), self.getstr(awaiting))
+                embed.add_field(name="Awaiting confirmation", value=(len(awaiting)))
             if len(initi) != 0:
-                print("Initialising\t\t", "{0:0=3d}".format(len(initi)), self.getstr(initi))
+                #print("Initialising\t\t", "{0:0=3d}".format(len(initi)), self.getstr(initi))
+                embed.add_field(name="Initialising", value=(len(initi)))
             if len(initd) != 0:
-                print("Initialised\t\t", "{0:0=3d}".format(len(initd)), self.getstr(initd))
+                #print("Initialised\t\t", "{0:0=3d}".format(len(initd)), self.getstr(initd))
+                embed.add_field(name="Initialised", value=(len(initd)))
             if len(log) != 0:
-                print("Logging in\t", "{0:0=3d}".format(len(log)), "\t", self.getstr(log))
+                #print("Logging in\t", "{0:0=3d}".format(len(log)), "\t", self.getstr(log))
+                embed.add_field(name="Logging in", value=(len(log)))
             if len(webs) != 0:
-                print("Connect to webs", "{0:0=3d}".format(len(webs)), self.getstr(webs))
+                #print("Connect to webs", "{0:0=3d}".format(len(webs)), self.getstr(webs))
+                embed.add_field(name="Connecting to websocket", value=(len(webs)))
             if len(ident) != 0:
-                print("Identifying\t\t", "{0:0=3d}".format(len(ident)), self.getstr(ident))
+                #print("Identifying\t\t", "{0:0=3d}".format(len(ident)), self.getstr(ident))
+                embed.add_field(name="Identifying", value=(len(ident)))
             if len(recon) != 0:
-                print("Reconnecting\t\t", "{0:0=3d}".format(len(recon)), self.getstr(recon))
+                #print("Reconnecting\t\t", "{0:0=3d}".format(len(recon)), self.getstr(recon))
+                embed.add_field(name="Reconnecting", value=(len(recon)))
             if len(attempt) != 0:
-                print("Attempt to reconn", "{0:0=3d}".format(len(attempt)), self.getstr(attempt))
+                #print("Attempt to reconn", "{0:0=3d}".format(len(attempt)), self.getstr(attempt))
+                embed.add_field(name="Attempting to reconnect", value=(len(attempt)))
             if len(missing) != 0:
-                print("Data missing", "{0:0=3d}".format(len(missing)), self.getstr(missing))
+                #print("Data missing", "{0:0=3d}".format(len(missing)), self.getstr(missing))
+                embed.add_field(name="Data missing", value=(len(missing)))
             await ctx.send(embed=embed)
         print("------------------------------")
         print("By cluster")
