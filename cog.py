f8ac48241cc0d3b35e62b4355600e764d4d532db
aejb <Bakehouse01>
2018-12-05 10:20:05 +0000

    cleaned and refactored

--- cog.py
+++ cog.py
@@ -1,226 +1,225 @@
 __authors__ = 'aejb'
 import discord
 from discord.ext import commands
-import sqlite3
-import os
-import subprocess
 import requests
-import sys
-from bs4 import BeautifulSoup
 import json
 import math
 import datetime
 
-class shellcog:
+
+class ShellCog:
 
     def __init__(self, bot):
         self.bot = bot
-        self.shardcount=2304
-
-    def getstr(self, arre):
-        if len(arre)>5:
-            return "..."
-        elif len(arre)==0:
-            return ""
-        else:
-            arre_s=">"
-            for i in arre:
-                arre_s="\t"+arre_s+i+", "
-            return arre_s
+        self.shardCount = 2304
 
     @commands.command()
     async def help(self, ctx):
-        await ctx.channel.send("**Command**\t\t\t\t\t\t\t**Aliases**\t\t**Description**\n`ri hello`\n`ri help`\t\t\t\t\t\t\t\tNone\t\t  This command\n`ri guild <guild_ID>`\t`g`\t\t\t\t  Shard and Cluster information from the given Guild ID\n`ri shard <shard_ID>`\t`s`\t\t\t\t  Shard and Cluster information from the given Shard ID\n`ri cluster`\t\t\t\t\t\t `c`\t\t\t\t  Informaion on shard issues sorted by cluster\n`ri status`\t\t\t\t\t\t   `info, i`\t Information on shard issues by issue type")
+        await ctx.channel.send("**Command**\t\t\t\t\t\t\t**Aliases**\t\t**Description**\n`ri hello`\n`ri "
+                               "help`\t\t\t\t\t\t\t\tNone\t\t  This command\n`ri guild <guild_ID>`\t`g`\t\t\t\t  "
+                               "Shard and Cluster information from the given Guild ID\n`ri shard "
+                               "<shard_ID>`\t`s`\t\t\t\t  Shard and Cluster information from the given Shard ID\n`ri "
+                               "cluster`\t\t\t\t\t\t `c`\t\t\t\t  Informaion on shard issues sorted by cluster\n`ri "
+                               "status`\t\t\t\t\t\t   `info, i`\t Information on shard issues by issue type")
 
     @commands.command()
     async def hello(self, ctx):
         await ctx.channel.send("oh hi")
 
     @commands.command(aliases=["g"])
-    async def guild(self, ctx, guild_ID: int):
-        url_combined = str("https://web.rythmbot.co/ajax/shard/" + str(guild_ID))
+    async def guild(self, ctx, guild_id: int):
+        url_combined = str("https://web.rythmbot.co/ajax/shard/" + str(guild_id))
         page = requests.get(url_combined)
         python_obj = json.loads(page.text)
-        shard_ID=int(python_obj["shard"])
-        cluster_ID=int(python_obj["cluster"])
+        shard_id = int(python_obj["shard"])
+        cluster_id = int(python_obj["cluster"])
         embed = discord.Embed(colour=discord.Colour(0xd0892f), description="Info from Guild")
-        embed.add_field(name="Guild", value=guild_ID, inline=False)
-        embed.add_field(name="Shard", value=shard_ID, inline=False)
-        embed.add_field(name="Cluster", value=cluster_ID, inline=False)
-        rawstat = requests.get("https://status.rythmbot.co/raw")
-        embed.add_field(name="Status", value=json.loads(rawstat.text)[str(shard_ID)], inline=False)
+        embed.add_field(name="Guild", value=str(guild_id), inline=False)
+        embed.add_field(name="Shard", value=str(shard_id), inline=False)
+        embed.add_field(name="Cluster", value=str(cluster_id), inline=False)
+        raw_stat = requests.get("https://status.rythmbot.co/raw")
+        embed.add_field(name="Status", value=json.loads(raw_stat.text)[str(shard_id)], inline=False)
         embed.set_footer(text="a bot by ash#0001")
         await ctx.send(embed=embed)
 
     @commands.command(aliases=["s"])
-    async def shard(self, ctx, shard_ID: int):
+    async def shard(self, ctx, shard_id: int):
         embed = discord.Embed(colour=discord.Colour(0xd0892f), description="Info from Shard")
-        embed.add_field(name="Shard", value=shard_ID, inline=False)
-        embed.add_field(name="Cluster", value=math.floor(int(shard_ID)/int(math.ceil(self.shardcount/9))), inline=False)
-        rawstat = requests.get("https://status.rythmbot.co/raw")
-        embed.add_field(name="Status", value=json.loads(rawstat.text)[str(shard_ID)], inline=False)
+        embed.add_field(name="Shard", value=str(shard_id), inline=False)
+        embed.add_field(name="Cluster", value=str(math.floor(int(shard_id) / int(math.ceil(self.shardCount / 9)))),
+                        inline=False)
+        raw_stat = requests.get("https://status.rythmbot.co/raw")
+        embed.add_field(name="Status", value=json.loads(raw_stat.text)[str(shard_id)], inline=False)
         embed.set_footer(text="a bot by ash#0001")
         await ctx.send(embed=embed)
 
     @commands.command(aliases=["c"])
     async def cluster(self, ctx):
-        onlinecount=0
-        issues_array=[]
-        #!debug rawstat = requests.get("http://cdn.dvorak.host/test.json")
-        rawstat = requests.get("https://status.rythmbot.co/raw")
-        raw=json.loads(rawstat.text)
+        onlinecount = 0
+        issues_array = []
+        # debug raw_stat = requests.get("http://cdn.dvorak.host/test.json")
+        raw_stat = requests.get("https://status.rythmbot.co/raw")
+        raw = json.loads(raw_stat.text)
         for i in raw:
-            if raw[str(i)]=="CONNECTED":
-                onlinecount=onlinecount+1
+            if raw[str(i)] == "CONNECTED":
+                onlinecount = onlinecount + 1
             else:
-                shard_ID=int(i)
-                cluster_ID=math.floor(int(shard_ID)/int(math.ceil(self.shardcount/9)))
-                issues_array.append([shard_ID, cluster_ID])
-        cluster0=[]
-        cluster1=[]
-        cluster2=[]
-        cluster3=[]
-        cluster4=[]
-        cluster5=[]
-        cluster6=[]
-        cluster7=[]
-        cluster8=[]
-        cluster9=[]
+                shard_id = int(i)
+                cluster_id = math.floor(int(shard_id) / int(math.ceil(self.shardCount / 9)))
+                issues_array.append([shard_id, cluster_id])
+        cluster0 = []
+        cluster1 = []
+        cluster2 = []
+        cluster3 = []
+        cluster4 = []
+        cluster5 = []
+        cluster6 = []
+        cluster7 = []
+        cluster8 = []
+        cluster9 = []
         for j in issues_array:
-            if (j[1])==0:
+            if (j[1]) == 0:
                 cluster0.append(j)
-            if (j[1])==1:
+            if (j[1]) == 1:
                 cluster1.append(j)
-            if (j[1])==2:
+            if (j[1]) == 2:
                 cluster2.append(j)
-            if (j[1])==3:
+            if (j[1]) == 3:
                 cluster3.append(j)
-            if (j[1])==4:
+            if (j[1]) == 4:
                 cluster4.append(j)
-            if (j[1])==5:
+            if (j[1]) == 5:
                 cluster5.append(j)
-            if (j[1])==6:
+            if (j[1]) == 6:
                 cluster6.append(j)
-            if (j[1])==7:
+            if (j[1]) == 7:
                 cluster7.append(j)
-            if (j[1])==8:
+            if (j[1]) == 8:
                 cluster8.append(j)
-            if (j[1])==9:
+            if (j[1]) == 9:
                 cluster9.append(j)
-        problems=self.shardcount-onlinecount
-        embed = discord.Embed(colour=discord.Colour(0xd0892f), description="Rythm is {}% Online\nThere are {} issues".format(str(round(((onlinecount)/self.shardcount), 1)*100), problems))
+        problems = self.shardCount - onlinecount
+        embed = discord.Embed(colour=discord.Colour(0xd0892f),
+                              description="Rythm is {}% Online\nThere are {} issues".format(
+                                  str(round((onlinecount / self.shardCount), 1) * 100), problems))
         embed.set_author(name="Rythm Cluster Status")
-        if len(cluster0)!=0:
-            embed.add_field(name="Cluster 0", value=(len(cluster0)), inline=False)
-        if len(cluster1)!=0:
-            embed.add_field(name="Cluster 1", value=(len(cluster1)), inline=False)
-        if len(cluster2)!=0:
-            embed.add_field(name="Cluster 2", value=(len(cluster2)), inline=False)
-        if len(cluster3)!=0:
-            embed.add_field(name="Cluster 3", value=(len(cluster3)), inline=False)
-        if len(cluster4)!=0:
-            embed.add_field(name="Cluster 4", value=(len(cluster4)), inline=False)
-        if len(cluster5)!=0:
-            embed.add_field(name="Cluster 5", value=(len(cluster5)), inline=False)
-        if len(cluster6)!=0:
-            embed.add_field(name="Cluster 6", value=(len(cluster6)), inline=False)
-        if len(cluster7)!=0:
-            embed.add_field(name="Cluster 7", value=(len(cluster7)), inline=False)
-        if len(cluster8)!=0:
-            embed.add_field(name="Cluster 8", value=(len(cluster8)), inline=False)
-        if len(cluster9)!=0:
-            embed.add_field(name="Cluster 9", value=(len(cluster9)), inline=False)
+        if len(cluster0) != 0:
+            embed.add_field(name="Cluster 0", value=str((len(cluster0))), inline=False)
+        if len(cluster1) != 0:
+            embed.add_field(name="Cluster 1", value=str((len(cluster1))), inline=False)
+        if len(cluster2) != 0:
+            embed.add_field(name="Cluster 2", value=str((len(cluster2))), inline=False)
+        if len(cluster3) != 0:
+            embed.add_field(name="Cluster 3", value=str((len(cluster3))), inline=False)
+        if len(cluster4) != 0:
+            embed.add_field(name="Cluster 4", value=str((len(cluster4))), inline=False)
+        if len(cluster5) != 0:
+            embed.add_field(name="Cluster 5", value=str((len(cluster5))), inline=False)
+        if len(cluster6) != 0:
+            embed.add_field(name="Cluster 6", value=str((len(cluster6))), inline=False)
+        if len(cluster7) != 0:
+            embed.add_field(name="Cluster 7", value=str((len(cluster7))), inline=False)
+        if len(cluster8) != 0:
+            embed.add_field(name="Cluster 8", value=str((len(cluster8))), inline=False)
+        if len(cluster9) != 0:
+            embed.add_field(name="Cluster 9", value=str((len(cluster9))), inline=False)
         embed.set_footer(text="a bot by ash#0001")
+        time_to_seconds = int(problems * 6.5)
+        time_in_minutes = str(datetime.timedelta(seconds=time_to_seconds))
+        embed.add_field(name="Expected Resolution Time", value=time_in_minutes, inline=False)
         await ctx.send(embed=embed)
 
-    ## RYTHM INFO
+    # RYTHM INFO
 
     @commands.command(aliases=["info", "i"])
     async def status(self, ctx):
-        onlinecount=0
-        initi=[]
-        initd=[]
-        log=[]
-        webs=[]
-        ident=[]
-        awaiting=[]
-        load=[]
-        recon=[]
-        attempt=[]
-        waitcon=[]
-        queue=[]
-        missing=[]
-        rawstat = requests.get("https://status.rythmbot.co/raw")
-        #!debug rawstat = requests.get("http://cdn.dvorak.host/test.json")
-        raw=json.loads(rawstat.text)
+        onlinecount = 0
+        initi = []
+        initd = []
+        log = []
+        webs = []
+        ident = []
+        awaiting = []
+        load = []
+        recon = []
+        attempt = []
+        waitcon = []
+        queue = []
+        missing = []
+        raw_stat = requests.get("https://status.rythmbot.co/raw")
+        # debug raw_stat = requests.get("http://cdn.dvorak.host/test.json")
+        raw = json.loads(raw_stat.text)
         for i in raw:
-            if raw[str(i)]=="CONNECTED":
-                onlinecount=onlinecount+1
-            elif raw[str(i)]=="INITIALIZING":
+            if raw[str(i)] == "CONNECTED":
+                onlinecount = onlinecount + 1
+            elif raw[str(i)] == "INITIALIZING":
                 initi.append(str(i))
-            elif raw[str(i)]=="INITIALIZED":
+            elif raw[str(i)] == "INITIALIZED":
                 initd.append(str(i))
-            elif raw[str(i)]=="LOGGING_IN":
+            elif raw[str(i)] == "LOGGING_IN":
                 log.append(str(i))
-            elif raw[str(i)]=="CONNECTING_TO_WEBSOCKET":
+            elif raw[str(i)] == "CONNECTING_TO_WEBSOCKET":
                 webs.append(str(i))
-            elif raw[str(i)]=="IDENTIFYING_SESSION":
+            elif raw[str(i)] == "IDENTIFYING_SESSION":
                 ident.append(str(i))
-            elif raw[str(i)]=="AWAITING_LOGIN_CONFIRMATION":
+            elif raw[str(i)] == "AWAITING_LOGIN_CONFIRMATION":
                 awaiting.append(str(i))
-            elif raw[str(i)]=="LOADING_SUBSYSTEMS":
+            elif raw[str(i)] == "LOADING_SUBSYSTEMS":
                 load.append(str(i))
-            elif raw[str(i)]=="RECONNECTING":
+            elif raw[str(i)] == "RECONNECTING":
                 recon.append(str(i))
-            elif raw[str(i)]=="ATTEMPTING_TO_RECONNECT":
+            elif raw[str(i)] == "ATTEMPTING_TO_RECONNECT":
                 attempt.append(str(i))
-            elif raw[str(i)]=="WAITING_TO_RECONNECT":
+            elif raw[str(i)] == "WAITING_TO_RECONNECT":
                 waitcon.append(str(i))
-            elif raw[str(i)]=="RECONNECT_QUEUED":
+            elif raw[str(i)] == "RECONNECT_QUEUED":
                 queue.append(str(i))
             else:
                 missing.append(str(i))
-        if onlinecount==self.shardcount:
-            embed = discord.Embed(colour=discord.Colour(0xd0892f), description="Rythm is 100% Online\nThere are 0 issues")
+        if onlinecount == self.shardCount:
+            embed = discord.Embed(colour=discord.Colour(0xd0892f),
+                                  description="Rythm is 100% Online\nThere are 0 issues")
             embed.set_author(name="Rythm Status")
             embed.set_footer(text="a bot by ash#0001")
             await ctx.send(embed=embed)
 
         else:
-            problems=self.shardcount-onlinecount
-            embed = discord.Embed(colour=discord.Colour(0xd0892f), description="Rythm is {}% Online\nThere are {} issues".format(str(round(((onlinecount)/self.shardcount), 1)*100), problems))
+            problems = self.shardCount - onlinecount
+            embed = discord.Embed(colour=discord.Colour(0xd0892f),
+                                  description="Rythm is {}% Online\nThere are {} issues".format(
+                                      str(round((onlinecount / self.shardCount), 1) * 100), problems))
             embed.set_author(name="Rythm Status")
             embed.set_footer(text="a bot by ash#0001")
             if len(queue) != 0:
-                embed.add_field(name="Reconnect queue", value=(len(queue)), inline=False)
+                embed.add_field(name="Reconnect queue", value=str((len(queue))), inline=False)
             if len(waitcon) != 0:
-                embed.add_field(name="Waiting to reconnect", value=(len(waitcon)), inline=False)
+                embed.add_field(name="Waiting to reconnect", value=str((len(waitcon))), inline=False)
             if len(load) != 0:
-                embed.add_field(name="Loading subsystems", value=(len(load)), inline=False)
+                embed.add_field(name="Loading subsystems", value=str((len(load))), inline=False)
             if len(awaiting) != 0:
-                embed.add_field(name="Awaiting confirmation", value=(len(awaiting)), inline=False)
+                embed.add_field(name="Awaiting confirmation", value=str((len(awaiting))), inline=False)
             if len(initi) != 0:
-                embed.add_field(name="Initialising", value=(len(initi)), inline=False)
+                embed.add_field(name="Initialising", value=str((len(initi))), inline=False)
             if len(initd) != 0:
-                embed.add_field(name="Initialised", value=(len(initd)), inline=False)
+                embed.add_field(name="Initialised", value=str((len(initd))), inline=False)
             if len(log) != 0:
-                embed.add_field(name="Logging in", value=(len(log)), inline=False)
+                embed.add_field(name="Logging in", value=str((len(log))), inline=False)
             if len(webs) != 0:
-                embed.add_field(name="Connecting to websocket", value=(len(webs)), inline=False)
+                embed.add_field(name="Connecting to websocket", value=str((len(webs))), inline=False)
             if len(ident) != 0:
-                embed.add_field(name="Identifying", value=(len(ident)), inline=False)
+                embed.add_field(name="Identifying", value=str((len(ident))), inline=False)
             if len(recon) != 0:
-                embed.add_field(name="Reconnecting", value=(len(recon)), inline=False)
+                embed.add_field(name="Reconnecting", value=str((len(recon))), inline=False)
             if len(attempt) != 0:
-                embed.add_field(name="Attempting to reconnect", value=(len(attempt)), inline=False)
+                embed.add_field(name="Attempting to reconnect", value=str((len(attempt))), inline=False)
             if len(missing) != 0:
-                embed.add_field(name="Data missing", value=(len(missing)), inline=False)
-            timeToSeconds=int(problems*6.5)
-            timeInMinutes=str(datetime.timedelta(seconds=timeToSeconds))
-            embed.add_field(name="Expected Resolution Time", value=timeInMinutes, inline=False)
+                embed.add_field(name="Data missing", value=str((len(missing))), inline=False)
+            time_to_seconds = int(problems * 6.5)
+            time_in_minutes = str(datetime.timedelta(seconds=time_to_seconds))
+            embed.add_field(name="Expected Resolution Time", value=time_in_minutes, inline=False)
             await ctx.send(embed=embed)
 
 
 def setup(bot):
     bot.remove_command("help")
-    bot.add_cog(shellcog(bot))
+    bot.add_cog(ShellCog(bot))
