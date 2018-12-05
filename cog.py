ddf01251e8ff1f77bce48569a789b5e408881c54
aejb <Bakehouse01>
2018-12-05 17:39:08 +0000

    testing cluster info

--- cog.py
+++ cog.py
@@ -26,6 +26,94 @@ class ShellCog:
     async def hello(self, ctx):
         await ctx.channel.send("oh hi")
 
+    @commands.command(alises="ci")
+    async def clusterinfo(self, ctx, cluster_choice: int):
+        onlinecount = 0
+        initi = []
+        initd = []
+        log = []
+        webs = []
+        ident = []
+        awaiting = []
+        load = []
+        recon = []
+        attempt = []
+        waitcon = []
+        queue = []
+        missing = []
+        raw_stat = requests.get("http://cdn.dvorak.host/test.json")
+        # raw_stat = requests.get("https://status.rythmbot.co/raw")
+        raw = json.loads(raw_stat.text)
+        for i in raw:
+           if math.floor(int(shard_id) / int(math.ceil(self.shardCount / 9))) == cluster_choice:
+               if raw[str(i)] == "CONNECTED":
+                   onlinecount = onlinecount + 1
+               elif raw[str(i)] == "INITIALIZING":
+                   initi.append(str(i))
+               elif raw[str(i)] == "INITIALIZED":
+                   initd.append(str(i))
+               elif raw[str(i)] == "LOGGING_IN":
+                   log.append(str(i))
+               elif raw[str(i)] == "CONNECTING_TO_WEBSOCKET":
+                   webs.append(str(i))
+               elif raw[str(i)] == "IDENTIFYING_SESSION":
+                   ident.append(str(i))
+               elif raw[str(i)] == "AWAITING_LOGIN_CONFIRMATION":
+                   awaiting.append(str(i))
+               elif raw[str(i)] == "LOADING_SUBSYSTEMS":
+                   load.append(str(i))
+               elif raw[str(i)] == "RECONNECTING":
+                   recon.append(str(i))
+               elif raw[str(i)] == "ATTEMPTING_TO_RECONNECT":
+                   attempt.append(str(i))
+               elif raw[str(i)] == "WAITING_TO_RECONNECT":
+                   waitcon.append(str(i))
+               elif raw[str(i)] == "RECONNECT_QUEUED":
+                   queue.append(str(i))
+               else:
+                   missing.append(str(i))
+           else:
+               return
+        if onlinecount == self.shardCount:
+            embed = discord.Embed(colour=discord.Colour(0xd0892f),
+                                  description="Rythm Cluster {}% is 100% Online\nThere are 0 issues".format(cluster_choice))
+            embed.set_author(name="Rythm Cluster {}% Status".format(cluster_choice))
+            embed.set_footer(text="a bot by ash#0001")
+            await ctx.send(embed=embed)
+        else:
+            problems = self.shardCount - onlinecount
+            embed = discord.Embed(colour=discord.Colour(0xd0892f),
+                                  description="Rythm Cluster {}% is {}% Online\nThere are {} issues".format(cluster_choice,
+                                      str(round((onlinecount / self.shardCount), 1) * 100), problems))
+            embed.set_author(name="Rythm Cluster {}% Status".format(cluster_choice))
+            embed.set_footer(text="a bot by ash#0001")
+            if len(queue) != 0:
+                embed.add_field(name="Reconnect queue", value=str((len(queue))), inline=False)
+            if len(waitcon) != 0:
+                embed.add_field(name="Waiting to reconnect", value=str((len(waitcon))), inline=False)
+            if len(load) != 0:
+                embed.add_field(name="Loading subsystems", value=str((len(load))), inline=False)
+            if len(awaiting) != 0:
+                embed.add_field(name="Awaiting confirmation", value=str((len(awaiting))), inline=False)
+            if len(initi) != 0:
+                embed.add_field(name="Initialising", value=str((len(initi))), inline=False)
+            if len(initd) != 0:
+                embed.add_field(name="Initialised", value=str((len(initd))), inline=False)
+            if len(log) != 0:
+                embed.add_field(name="Logging in", value=str((len(log))), inline=False)
+            if len(webs) != 0:
+                embed.add_field(name="Connecting to websocket", value=str((len(webs))), inline=False)
+            if len(ident) != 0:
+                embed.add_field(name="Identifying", value=str((len(ident))), inline=False)
+            if len(recon) != 0:
+                embed.add_field(name="Reconnecting", value=str((len(recon))), inline=False)
+            if len(attempt) != 0:
+                embed.add_field(name="Attempting to reconnect", value=str((len(attempt))), inline=False)
+            if len(missing) != 0:
+                embed.add_field(name="Data missing", value=str((len(missing))), inline=False)
+            await ctx.send(embed=embed)
+
+
     @commands.command(aliases=["g"])
     async def guild(self, ctx, guild_id: int):
         url_combined = str("https://web.rythmbot.co/ajax/shard/" + str(guild_id))
