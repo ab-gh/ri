2102de540569dead23df0b060dcad1db0972ca55
aejb <thomas.bass2001@gmail.com>
2019-03-04 19:47:46 +0000

    prod

--- cog.py
+++ cog.py
@@ -82,56 +82,29 @@ class ShellCog:
         if not cluster_choice:
             await ctx.channel.send('You need to specify cluster number')
         await ctx.channel.send('Loading...', delete_after=3)
-        onlinecount = 0
-        found_count = 0
-        initi = []
-        initd = []
-        log = []
-        webs = []
-        ident = []
-        awaiting = []
-        load = []
-        recon = []
-        attempt = []
-        waitcon = []
-        queue = []
-        missing = []
-        # raw_stat = requests.get("http://cdn.dvorak.host/test.json")
         raw = await self.getJSON(ctx)
-        # print("finished array setup")
+        found_count = 0
+        onlinecount = 0
+        missing  = []
+        status_dict = {"INITIALIZING": [], "INITIALIZED": [], "LOGGING_IN": [], "CONNECTING_TO_WEBSOCKET": [],
+                       "IDENTIFYING_SESSION": [], "AWAITING_LOGIN_CONFIRMATION": [], "LOADING_SUBSYSTEMS": [],
+                       "RECONNECTING": [], "ATTEMPTING_TO_RECONNECT": [], "WAITING_TO_RECONNECT": [],
+                       "RECONNECT_QUEUED": []}
+        string_dict = {"INITIALIZING": "Initialising", "INITIALIZED": "Initialised", "LOGGING_IN": "Logging in",
+                       "CONNECTING_TO_WEBSOCKET": "connecting to websocket", "IDENTIFYING_SESSION": "Identifying",
+                       "AWAITING_LOGIN_CONFIRMATION": "Awaiting confirmation",
+                       "LOADING_SUBSYSTEMS": "Loading subsystems", "RECONNECTING": "Reconnecting",
+                       "ATTEMPTING_TO_RECONNECT": "Attempting to reconnect",
+                       "WAITING_TO_RECONNECT": "Waiting to reconnect", "RECONNECT_QUEUED": "In reconnect queue"}
         for i in raw:
             if math.floor(int(i) / int(math.ceil(self.shardCount / 9))) == cluster_choice:
-                # print("in cluster")
                 found_count = found_count + 1
                 if raw[str(i)] == "CONNECTED":
-                    onlinecount = onlinecount + 1
-                elif raw[str(i)] == "INITIALIZING":
-                    initi.append(str(i))
-                elif raw[str(i)] == "INITIALIZED":
-                    initd.append(str(i))
-                elif raw[str(i)] == "LOGGING_IN":
-                    log.append(str(i))
-                elif raw[str(i)] == "CONNECTING_TO_WEBSOCKET":
-                    webs.append(str(i))
-                elif raw[str(i)] == "IDENTIFYING_SESSION":
-                    ident.append(str(i))
-                elif raw[str(i)] == "AWAITING_LOGIN_CONFIRMATION":
-                    awaiting.append(str(i))
-                elif raw[str(i)] == "LOADING_SUBSYSTEMS":
-                    load.append(str(i))
-                elif raw[str(i)] == "RECONNECTING":
-                    recon.append(str(i))
-                elif raw[str(i)] == "ATTEMPTING_TO_RECONNECT":
-                    attempt.append(str(i))
-                elif raw[str(i)] == "WAITING_TO_RECONNECT":
-                    waitcon.append(str(i))
-                elif raw[str(i)] == "RECONNECT_QUEUED":
-                    queue.append(str(i))
+                    onlinecount += 1
+                elif raw[str(i)] in status_dict:
+                    status_dict[raw[str(i)]].append(str(i))
                 else:
                     missing.append(str(i))
-        # else:
-        # print("not in cluster")
-        # print("finished checks")
         if onlinecount == found_count:
             embed = discord.Embed(colour=discord.Colour(0xd0892f),
                                   description="Rythm Cluster {} is 100% Online\nThere are 0 issues".format(
@@ -147,30 +120,11 @@ class ShellCog:
                                       str(round((onlinecount / self.shardCount), 1) * 100), problems))
             embed.set_author(name="Rythm Cluster {} Status".format(cluster_choice))
             embed.set_footer(text="a bot by ash#0001")
-            if len(queue) != 0:
-                embed.add_field(name="Reconnect queue", value=str((len(queue))), inline=False)
-            if len(waitcon) != 0:
-                embed.add_field(name="Waiting to reconnect", value=str((len(waitcon))), inline=False)
-            if len(load) != 0:
-                embed.add_field(name="Loading subsystems", value=str((len(load))), inline=False)
-            if len(awaiting) != 0:
-                embed.add_field(name="Awaiting confirmation", value=str((len(awaiting))), inline=False)
-            if len(initi) != 0:
-                embed.add_field(name="Initialising", value=str((len(initi))), inline=False)
-            if len(initd) != 0:
-                embed.add_field(name="Initialised", value=str((len(initd))), inline=False)
-            if len(log) != 0:
-                embed.add_field(name="Logging in", value=str((len(log))), inline=False)
-            if len(webs) != 0:
-                embed.add_field(name="Connecting to websocket", value=str((len(webs))), inline=False)
-            if len(ident) != 0:
-                embed.add_field(name="Identifying", value=str((len(ident))), inline=False)
-            if len(recon) != 0:
-                embed.add_field(name="Reconnecting", value=str((len(recon))), inline=False)
-            if len(attempt) != 0:
-                embed.add_field(name="Attempting to reconnect", value=str((len(attempt))), inline=False)
-            if len(missing) != 0:
-                embed.add_field(name="Data missing", value=str((len(missing))), inline=False)
+            for selection in status_dict:
+                if len(status_dict[selection]) != 0:
+                    embed.add_field(name=string_dict[selection], value=str((len(status_dict[selection]))), inline=False)
+                elif len(missing) != 0:
+                    embed.add_field(name="Data missing", value=str((len(missing))), inline=False)
             await ctx.send(embed=embed)
 
     # RYTHM GUILD INFO
@@ -252,8 +206,6 @@ class ShellCog:
     async def status(self, ctx):
         await ctx.channel.send('Loading...', delete_after=3)
         onlinecount = 0
-        # raw_stat = requests.get("https://status.rythmbot.co/raw")
-        # raw_stat = requests.get("http://cdn.dvorak.host/test.json")
         raw = await self.getJSON(ctx)
         status_dict = {"INITIALIZING": [], "INITIALIZED": [], "LOGGING_IN": [], "CONNECTING_TO_WEBSOCKET": [],
                        "IDENTIFYING_SESSION": [], "AWAITING_LOGIN_CONFIRMATION": [], "LOADING_SUBSYSTEMS": [],
@@ -267,15 +219,11 @@ class ShellCog:
                        "WAITING_TO_RECONNECT": "Waiting to reconnect", "RECONNECT_QUEUED": "In reconnect queue"}
         missing = []
         for i in raw:
-            #print(raw[str(i)])
             if raw[str(i)] == "CONNECTED":
                 onlinecount += 1
             elif raw[str(i)] in status_dict:
-                print(raw[str(i)])
                 status_dict[raw[str(i)]].append(str(i))
             else:
-                print(raw[str(i)])
-                print("missing")
                 missing.append(str(i))
         if onlinecount == self.shardCount:
             embed = discord.Embed(colour=discord.Colour(0xd0892f),
@@ -285,7 +233,6 @@ class ShellCog:
             await ctx.send(embed=embed)
         else:
             problems = self.shardCount - onlinecount
-            print("problems: ", problems)
             embed = discord.Embed(colour=discord.Colour(0xd0892f), description="Rythm is {}% Online\nThere are {} issues".format(round(((onlinecount / self.shardCount)*100), 4), problems))
             embed.set_author(name="Rythm Status")
             embed.set_footer(text="a bot by ash#0001")
