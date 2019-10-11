1d1f64f6a1d18f7be3c7609fd3f17979098e903f
aejb <aejb>
2019-10-11 12:39:10 +0100

    working on ri live

--- cog.py
+++ cog.py
@@ -39,21 +39,13 @@ class ShellCog(commands.Cog):
             embed.add_field(name="Rythm is currently {}% online", value="{} shards connected")
             print(embed)
             async with aiohttp.ClientSession() as session:
-                if self.testing == 0:
-                    async with session.get("http://10.10.10.61:1346/shardinfo") as response:
-                        print(response)
-                        if response.status == 200:
-                            raw = await response.text()
-                        else:
-                            message = "Error: HTTP error " + str(response.status)
-                            print(message)
-                else:
-                    async with session.get("http://cdn.dvorak.host/test.json") as response:
-                        if response.status == 200:
-                            raw = await response.text()
-                        else:
-                            message = "Error: HTTP error " + str(response.status)
-                            print(message)
+                async with session.get("http://10.10.10.61:1346/shardinfo") as response:
+                    print(response)
+                    if response.status == 200:
+                        raw = await response.text()
+                    else:
+                        message = "Error: HTTP error " + str(response.status)
+                        print(message)
                 raw_json = json.loads(raw)
             print(raw_json)
 
